2dcc28f8a9219adc21c6a0398eddbe6347962ead
PRCatarino <paulo061104@gmail.com>
2021-11-30 16:25:14 -0300

    coserto resize

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 (() => { 
     const funcaoResize = ()=>{
-        const {width:largura,height:altura}=screen; 
+        const {availWidth:largura,availHeight:altura}=screen; 
         const maior = largura > altura ? largura : altura 
-        //if(largura > altura){
-            document.getElementById("player").style.width = `${largura}px`;   
-            document.getElementById("player").style.height = `${altura}px`;
-        //}else{
-            //document.getElementById("player").style.width = `${maior*1.777}px`;   
-            //document.getElementById("player").style.height = `${maior}px`;
-        //}
-        console.log("passouAki")
         console.log(altura,largura)
+        console.log("passouAki")
+        if(largura > altura){
+            document.getElementById("player").style.width = `${maior}px`;   
+            document.getElementById("player").style.height = `${maior *0.5625}px`;      
+        }else{
+            document.getElementById("player").style.width = `${maior*1.777}px`;   
+            document.getElementById("player").style.height = `${maior}px`;
+        }
     } 
     window.addEventListener("resize", funcaoResize);
     funcaoResize();
