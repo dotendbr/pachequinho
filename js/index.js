a72da947190f4624d5e91ee900b88e98af9e7fe2
Wesdras Alves <wesdras.alves@gmail.com>
2021-12-07 09:14:47 -0300

    Ajustes no redimencionamento via javascript do video. Acerto nos divs e organização da estrutura do site

--- js/index.js
+++ js/index.js
@@ -1,19 +1,34 @@
 (() => { 
-    const funcaoResize = ()=>{
-        const {availWidth:largura,availHeight:altura}=screen; 
-        const maior = largura > altura ? largura : altura 
-        console.log(altura,largura)
-        console.log("passouAki")
-        if(largura > altura){
-            document.getElementById("player").style.width = `${maior}px`;   
-            document.getElementById("player").style.height = `${maior *0.5625}px`;      
-        }else{
-            document.getElementById("player").style.width = `${maior*1.777}px`;   
-            document.getElementById("player").style.height = `${maior}px`;
+    const resizeVideo = ()=>{
+        let timeout;
+        return () => {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                const elementIframe = document.querySelector('iframe');
+                const elementHeader = document.querySelector('header');
+                const elementContainer = document.querySelector('.container');
+                const elementBlockVideo = document.querySelector('#blockVideo');
+                const {innerWidth:largura,innerHeight:altura} = window;
+                
+                if(largura <= 1000){
+                    elementIframe.style.height = `${altura}px`;
+                    elementIframe.style.width = `${altura*1.777}px`;
+                    elementHeader.style.height = `${altura}px`;
+                    elementBlockVideo.style.height = `${altura}px`;
+                    elementContainer.style.height = `${altura}px`;
+                }
+                else{
+                    elementIframe.style.width = `${largura}px`;
+                    elementIframe.style.height = `${largura*0.5625}px`;
+                    elementHeader.style.height = `${largura*0.5625}px`;
+                    elementBlockVideo.style.height = `${largura*0.5625}px`;
+                    elementContainer.style.height = `${altura}px`;
+                }
+            }, 1000);
         }
     } 
-    window.addEventListener("resize", funcaoResize);
-    funcaoResize();
+    window.addEventListener("resize", resizeVideo());
+    resizeVideo()();
 })()
 
 
